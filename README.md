e356632879c56f877942375b70983fe4f2d4d30c
yumzhana <yumyumzhana@gmail.com>
2025-10-23 22:33:41 +0200

    Remove files and directories

--- README.md
+++
@@ -1,6 +0,0 @@
-## Subject: 
-
-# Study: 
-
-# Implementation : 
-
--- webserv_tester-main/config.py
+++
@@ -1,5 +0,0 @@
-SERVER_ADDR = '127.0.0.1'
-SERVER_PORT = 8083
-
-# if server receive uri longer than max uri length, it should reponse with 414
-MAX_URI_LENGTH = 1000
